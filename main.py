FIxing data leakage from preprocess

main.py
@@ -1,6 +1,7 @@
 from src.data_loader import load_data
 from src.preprocess import clean_data, select_column, normalize_data
 from src.window import create_window
+import numpy as np
 
 
 
@@ -9,7 +10,8 @@ def main():
 
     df_clean = clean_data(df_raw)
     df_select = select_column(df_clean)
-    df_normal, scaler = normalize_data(df_select)
+    split_raw = int(len(df_select) * 0.8)
+    df_normal, scaler = normalize_data(df_select, fit_end=split_raw)
 
     print(df_normal.head())
     print("Normalized shape:", df_normal.shape)
@@ -17,12 +19,19 @@ def main():
 
     #ubah jadi numpy array
     df_matrix = df_normal[["open","high","low","close","volume"]].values
-    X_train, y_train, X_test, y_test = create_window(df_matrix) 
+    X_train, y_train, X_test, y_test = create_window(df_matrix, split_index=split_raw) 
+
+
+    np.set_printoptions(precision=4, suppress=True)
 
     print("X_train: ", X_train.shape)
-    print("y_train: ", y_train.shape)
-    print("x_test: ", X_test.shape)
-    print("y_test: ", y_test.shape)
+    print("X_train visual: ")
+    print(X_train[1])
+    print("1 window shape:", X_train[0].shape)
+    print("1 timestep shape:", X_train[0, 0].shape)
+    #print("y_train: ", y_train.shape)
+    #print("x_test: ", X_test.shape)
+    #print("y_test: ", y_test.shape)
     
 
 

src/window.py
@@ -1,30 +1,20 @@
 import numpy as np
 
-def create_window(data_normal, window_size=30):
-    """
-    Membuat window time-series untuk LSTM.
-    Input: data_normal shape (n, 5)
-    - open, high, low, price, volume
+def create_window(data_normal, window_size=30, split_index=None):
+    X_train, y_train, X_test, y_test = [], [], [], []
 
-    Output:
-    - X (samples, window_size, features)
-    - y (samples,)
-    """
+    if split_index is None:
+        split_index = int(len(data_normal) * 0.8)
 
-    X = []
-    y = []
+    if split_index <= window_size:
+        raise ValueError("split_index harus > window_size")
 
-    
-    for i in range(window_size, len(data_normal)):
-        X.append(data_normal[i - window_size:i])  # 30 hari sebelumnya
-        y.append(data_normal[i, 3])  # kolom ke-3 = price (Close)
+    for i in range(window_size, split_index):
+        X_train.append(data_normal[i - window_size:i])
+        y_train.append(data_normal[i, 3])  # close
 
-    X = np.array(X)
-    y = np.array(y)
+    for i in range(split_index, len(data_normal)):
+        X_test.append(data_normal[i - window_size:i])
+        y_test.append(data_normal[i, 3])
 
-    # split train test 80%
-    split = int(len(X) * 0.8)
-    X_train, X_test = X[:split], X[split:]
-    y_train, y_test = y[:split], y[split:]
-
-    return X_train, y_train, X_test, y_test
+    return np.array(X_train), np.array(y_train), np.array(X_test), np.array(y_test)